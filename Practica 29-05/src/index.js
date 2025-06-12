4f876a115873dd2a8f2fa987f80b0ec870110e91
Guidit0u <guidocucciolettit2@gmail.com>
2025-06-12 11:02:50 -0300

    Add files via upload

---
+++ Practica 29-05/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function saludar(nombre) {
+    return `Hola, ${nombre}!`;
+}
+const usuario = "Guido";
+console.log(saludar(usuario));
---
+++ Practica 29-05/src/index.ts
@@ -0,0 +1,6 @@
+function saludar(nombre:string):string{
+    return `Hola, ${nombre}!`;
+}
+
+const usuario = "Guido";
+console.log(saludar(usuario));
